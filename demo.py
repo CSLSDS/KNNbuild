3a40de1c770e9b40683c0573cae28bccae189614
CSLSDS <56779420+CSLSDS@users.noreply.github.com>
2020-07-31 13:32:27 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,3 @@
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from knn import KNN
 from sklearn import datasets
 
 # load a common datascience toy dataset, containing iris species observations
@@ -12,28 +9,60 @@ x = iris.data
 #   that we are trying to predict
 y = iris.target
 
-# print the iris dataset information to add context
+# if you like, print the iris dataset information to add context
 print(iris['DESCR'])
 print('\n')
+
+# you could also explore the pandas library as a way of examining the dataframe
+import pandas as pd
+irisdf = pd.DataFrame(x)
+irisdf['class'] = y
+print(irisdf.shape)
+print(irisdf.head())
+
+####### PREPARATION #########
+from sklearn.model_selection import train_test_split
 
 # arrange x, y matrices into training (80% of data) & testing (20% remaining)
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-# instantiate knn model with 3 classes
+########################################################################################
+########## DIY KNN ############
+
+from knn import KNN
+
+# instantiate knn model with 3 classes for our 3 iris species
 knn = KNN(3)
 
 # 'fit' the model to our 'known' training subsets
 knn.fit(X_train, y_train)
 
 # predict class for 'unseen' test subset matrix of features without classes
-y_pred = knn.predict(X_test)
+# while storing it for later comparison
+our_prediction = knn.predict(X_test)
 
-# check and print accuracy of the constructed predictive modeling algorithm
-print(f"accuracy of homebrewed knn: {accuracy_score(y_test, y_pred)}\n")
+####### SKLEARN KNN #########
 
-# import sklearn's implementation and repeat for comparison
 from sklearn.neighbors import KNeighborsClassifier
+
+# our step 1:
+# knn = KNN(3)
 sklearn = KNeighborsClassifier(3)
+
+# our step 2:
+# knn.fit(X_train, y_train)
 sklearn.fit(X_train, y_train)
-pred = sklearn.predict(X_test)
-print(f"accuracy of sklearn library results: {accuracy_score(y_test, pred)}\n")
+
+# our step 3:
+# our_prediction = knn.predict(X_test)
+sklearn_prediction = sklearn.predict(X_test)
+
+####### COMPARE ACCURACY #######
+
+from sklearn.metrics import accuracy_score
+
+print(f"accuracy of homebrewed knn: {accuracy_score(y_test, our_prediction)}\n")
+
+print(f"accuracy of sklearn library results: {accuracy_score(y_test, sklearn_prediction)}\n")
+
+
